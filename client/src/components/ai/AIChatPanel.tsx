97df7d3de7cb92aae8924338bb05662391cdab89
jianbaobao <3146378701@qq.com>
2026-07-30 01:58:52 +0800

    fix: correct AIChatPanel apiClient import

--- client/src/components/ai/AIChatPanel.tsx
+++ client/src/components/ai/AIChatPanel.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { apiClient } from '../../utils/api'
+import apiClient from '../../utils/api'
 
 interface AIChatProps {
   terminalOutput?: string
